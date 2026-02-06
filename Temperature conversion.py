f0119f1c523f689e375a3b44348dbe0436a57af7
Brainy <juniorbrainy7@gmail.com>
2026-02-06 15:07:30 +0100

    Calculation added

---
+++ Temperature conversion.py
@@ -0,0 +1,7 @@
+#Write a program which prompts the user for a celcius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+#Formula: (C x 9/5) + 32 = F
+
+celcius = input("Enter temperature in celcius: ")
+F = (celcius * 9/5) + 32 
+
+
